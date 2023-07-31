1274e3788743c478a7e9d9cc8df8602a0c9a9d7b
Abdul Quadri <quadriabdul18@gmail.com>
2023-07-31 22:53:15 +0100

    Beginning of timekeepr basics only

---
+++ scripts.js
@@ -0,0 +1,65 @@
+var seconds = 5
+var timeoutId;
+var isPaused = false;
+var isStarted = false;
+var originalMinutes = 0;
+var minutes = 0;
+var alertShown = false; // New flag to track if the alert has been shown
+
+
+
+function countdown(){
+
+    var minutes = parseInt(document.getElementById("length").value)
+    timeoutId = setTimeout(countdown,1000);
+    
+
+
+    
+
+    if (originalMinutes === 0 && !isStarted) {
+        originalMinutes = minutes;
+        isStarted = true;
+    }
+
+    
+    var formatted_seconds = seconds < 10 ? "0" + seconds : seconds;                   
+    document.getElementById('time').innerHTML = originalMinutes + ":" +  formatted_seconds ;
+    
+    if(originalMinutes > 0 || seconds > 0 ){
+
+        if(seconds > 0){
+            seconds--;
+        }else{
+            if(originalMinutes > 0 ){
+                originalMinutes--
+                seconds = 5
+            }
+            
+        }
+                                                    
+    } else{
+        if (!alertShown) { // Show the alert only once
+            alertShown = true;
+            alert("Countdown completed!");
+            originalMinutes = 0;
+            isStarted = false; // Reset the countdown start flag
+            document.getElementById('time').innerHTML = originalMinutes + ":" +  formatted_seconds ; // Reset the display
+            countdown()
+        }
+    }
+
+    return false;
+}
+
+document.getElementById('pause-button').addEventListener('click', function () {
+    clearTimeout(timeoutId);
+    isPaused = true;
+});
+
+document.getElementById('start-button').addEventListener('click', function () {
+    isPaused = false;
+    countdown();
+});
+
+
