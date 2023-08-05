bb47b9567e11466981c674790262a6f8b5b64975
Abdul Quadri <quadriabdul18@gmail.com>
2023-08-05 22:35:12 +0100

    Now using SetInterval to optimise, uses less code

--- scripts.js
+++ scripts.js
@@ -1,65 +1,41 @@
-var seconds = 5
+
 var timeoutId;
 var isPaused = false;
 var isStarted = false; 
 var originalMinutes = 0;
-var minutes = 0;
+
 var alertShown = false; // New flag to track if the alert has been shown
 
+//  Using setInterval
 
+function start_timer(duration,display){
 
-function countdown(){
+    var timer = duration,minutes,seconds
 
-    var minutes = parseInt(document.getElementById("length").value)
-    timeoutId = setTimeout(countdown,1000);
-    
+    var intervalId = setInterval(function(){
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    
+        display.textContent = minutes + ":" + seconds;
 
-    if (originalMinutes === 0 && !isStarted) {
-        originalMinutes = minutes;
-        isStarted = true;
-    }
+        if (--timer < 0) {
+            timer = duration;
+        };
+    },1000);
 
-    
-    var formatted_seconds = seconds < 10 ? "0" + seconds : seconds;                   
-    document.getElementById('time').innerHTML = originalMinutes + ":" +  formatted_seconds ;
-    
-    if(originalMinutes > 0 || seconds > 0 ){
-
-        if(seconds > 0){
-            seconds--;
-        }else{
-            if(originalMinutes > 0 ){
-                originalMinutes--
-                seconds = 5
-            }
-            
-        }
-                                                    
-    } else{
-        if (!alertShown) { // Show the alert only once
-            alertShown = true;
-            alert("Countdown completed!");
-            originalMinutes = 0;
-            isStarted = false; // Reset the countdown start flag
-            document.getElementById('time').innerHTML = originalMinutes + ":" +  formatted_seconds ; // Reset the display
-            countdown()
-        }
-    }
-
-    return false;
+    document.getElementById('stop-button').addEventListener('click', function () {
+        clearInterval(intervalId);
+    });
 }
 
-document.getElementById('pause-button').addEventListener('click', function () {
-    clearTimeout(timeoutId);
-    isPaused = true;
-});
 
-document.getElementById('start-button').addEventListener('click', function () {
-    isPaused = false;
-    countdown();
-});
-
+    document.getElementById('start-button').addEventListener('click', function(){
+        
+        var count = 60 * parseInt(document.getElementById("length").value);
+        display = document.getElementById('time');
+        start_timer(count,display)
+    });                                                                                   
 
